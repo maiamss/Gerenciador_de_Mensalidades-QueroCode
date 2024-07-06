f5d8b9feffb912427ba9ffb4730c91304e9ae4f5
Maiara <maiara.ctt@gmail.com>
2024-07-06 19:17:42 -0300

    configurando o banco

--- db/migrate/20240703034533_create_enrollments.rb
+++ db/migrate/20240703034533_create_enrollments.rb
@@ -1,6 +1,7 @@
 class CreateEnrollments < ActiveRecord::Migration[7.1]
   def change
     create_table :enrollments do |t|
+      t.references :student, null: false, foreign_key: true
       t.integer :amount
       t.integer :installments
       t.integer :due_day
--- db/migrate/20240703034542_create_bills.rb
+++ db/migrate/20240703034542_create_bills.rb
@@ -1,6 +1,7 @@
 class CreateBills < ActiveRecord::Migration[7.1]
   def change
     create_table :bills do |t|
+      t.references :enrollment, null: false, foreign_key: true
       t.integer :amount
       t.date :due_date
       t.string :status
