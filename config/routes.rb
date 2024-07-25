fa333258926da4ccf8fbea47a70fa2e41c2e4de7
Maiara <maiara.ctt@gmail.com>
2024-07-25 12:39:13 -0300

    Mudando modelo de rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   namespace 'v1' do
-  
-    # Rota para listar todos os alunos
-    get "/students", to: "students#index"
-    
-    # Rota para cadastrar um novo aluno
-    post "/students", to: "students#create"
-    
+    resources :students, only: [:index, :create, :update, :destroy]
+    resources :enrollments, only: [:index, :create]
   end
 end
+
